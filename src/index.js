0b9958106994818b4cd9a5d6aa2e32b64ec314df
Vladimir <fayst_@bk.ru>
2020-09-25 18:20:42 +0300

    Деплой на GH-Pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Runner from './js/runner';
-import Select from './js/select';
-import Slider from  './js/slider';
-
-const runner = new Runner('block_runner', 'block_img__img');
-const select = new Select('block_inp__sel', 'block_inp__block_options');
-const slider = new Slider('block_control__inp', 'block_slider__slider_item');
